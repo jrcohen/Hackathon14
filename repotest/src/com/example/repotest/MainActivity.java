f0c7f39c9469f1de76e8aa67cd9ad713aae6f838
elliotsperling <elliotsperling@128.54.18.233>
2014-05-02 14:31:20 -0700

    Added: LoginActivity.java, activity_login.xml, string_activity_login.xml Edited: MainActivity.java (commented out 'savedInstanceState' and changed setContentView to show login page first)

--- repotest/src/com/example/repotest/MainActivity.java
+++ repotest/src/com/example/repotest/MainActivity.java
@@ -16,13 +16,14 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
+        setContentView(R.layout.activity_login);
+        
+        // commented out to make login work as first screen
+        /**if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
+        }*/
     }
 
 
