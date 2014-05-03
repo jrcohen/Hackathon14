67170ce5f0936858c7625f40354c146da9245e81
Kevin <Kevin@128.54.18.112>
2014-05-02 19:20:09 -0700

    Added Leaderboard Control/View

--- repotest/gen/com/example/repotest/R.java
+++ repotest/gen/com/example/repotest/R.java
@@ -1088,13 +1088,18 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
+        public static final int action_forgot_password=0x7f05004b;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f05004a;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f05003e;
+        public static final int button2=0x7f05003f;
+        public static final int button3=0x7f050040;
+        public static final int button4=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1111,9 +1116,14 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int leaderboard_title=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int login_form=0x7f050046;
+        public static final int login_status=0x7f050044;
+        public static final int login_status_message=0x7f050045;
         public static final int middle=0x7f050012;
+        public static final int name=0x7f050047;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
@@ -1134,9 +1144,13 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int sign_in_button=0x7f050049;
+        public static final int spinner=0x7f050048;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int tableRow1=0x7f050043;
+        public static final int table_1=0x7f050042;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1193,12 +1207,20 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_leaderboard=0x7f030018;
+        public static final int activity_login=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int activity_score=0x7f03001b;
+        public static final int fragment_leaderboard=0x7f03001c;
+        public static final int fragment_main=0x7f03001d;
+        public static final int fragment_score=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int leaderboard=0x7f0c0000;
+        public static final int login=0x7f0c0001;
+        public static final int main=0x7f0c0002;
+        public static final int score=0x7f0c0003;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1241,9 +1263,27 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int action_forgot_password=0x7f0a001a;
         public static final int action_settings=0x7f0a000f;
+        public static final int action_sign_in_register=0x7f0a0018;
+        public static final int action_sign_in_short=0x7f0a0019;
         public static final int app_name=0x7f0a000d;
+        public static final int error_field_required=0x7f0a001f;
+        public static final int error_incorrect_password=0x7f0a001e;
+        public static final int error_invalid_email=0x7f0a001c;
+        public static final int error_invalid_password=0x7f0a001d;
         public static final int hello_world=0x7f0a000e;
+        public static final int login_progress_signing_in=0x7f0a001b;
+        /**  Strings related to login 
+         */
+        public static final int prompt_name=0x7f0a0016;
+        public static final int select_group=0x7f0a0017;
+        public static final int table_1=0x7f0a0014;
+        public static final int title_activity_leaderboard=0x7f0a0010;
+        public static final int title_activity_login=0x7f0a0015;
+        public static final int title_section1=0x7f0a0011;
+        public static final int title_section2=0x7f0a0012;
+        public static final int title_section3=0x7f0a0013;
     }
     public static final class style {
         /** 
@@ -1272,6 +1312,7 @@ containing a value of this type.
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f0b008c;
+        public static final int LoginFormContainer=0x7f0b008d;
         /**  Mimic text appearance in select_dialog_item.xml 
          */
         public static final int TextAppearance_AppCompat_Base_CompactMenu_Dialog=0x7f0b0063;
